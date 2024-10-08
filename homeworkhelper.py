a2d34aaa8d59d85d6f29ca5778747009d4f16d5d
vini rodri <140485764+viniolt@users.noreply.github.com>
2024-10-08 05:03:58 -0300

    Add files via upload

---
+++ homeworkhelper.py
@@ -0,0 +1,28 @@
+import base64
+import openai
+
+openai.api_key = ""#YOUR OPENAI API KEY HERE
+MODEL = "chatgpt-4o-latest" #SET THE GPT MODEL HERE
+
+IMAGE_PATH = ("")#IMAGE PATH HERE (YOUR HOMEWORK, OR WHATEVER YOU WANT GPT TO READ)
+def encode_image(image_path): #Decoding image
+    with open(image_path, 'rb') as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+base64_image = encode_image(IMAGE_PATH)
+
+response = openai.ChatCompletion.create(
+    model=MODEL,
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant that responds in Markdown. Help me with my homework!"}, #GPT's role
+        {"role": "user", "content": [
+            {"type": "text", "text": "Insert here your prompt. Exemple: Find the area of the triangle"},
+            {"type": "image_url", "image_url": {
+                "url": f"data:image/png;base64,{base64_image}"}
+             }
+        ]}
+    ],
+    temperature=0.0,
+)
+
+print(response.choices[0].message.content)
